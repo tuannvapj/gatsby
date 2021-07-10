068f9311a0bce4c940e565843c6097c20ee81755
Tuan Nguyen <tuannva92@gmail.com>
2021-07-10 22:32:21 +0700

    Tạo header và style cho header

--- src/html.js
+++ src/html.js
@@ -27,6 +27,7 @@ export default function HTML(props) {
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+                <title>TUANNVA Travel | Testing</title>
                 {props.headComponents}
                 <link rel="icon" href={favicon} />
                 {css}
